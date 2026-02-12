3f742321d1aafafaaa88fd7df0b316a877516223
LorenzoL <leo.Lorre@gmx.de>
2026-02-12 19:45:19 +0100

    Fix import statements in Discord_Interface.py

--- bot/Discord_Interface.py
+++ bot/Discord_Interface.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
 
-from func import 
-import
+from func import
+import shelve
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -16,4 +16,5 @@ async def start(ctx):
 # read token and run with it
 
 bot.run(token)
+
 
