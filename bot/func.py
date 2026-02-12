14c7c9f06b91fb111d13de46352bbe2e9cfa87c8
LorenzoL <leo.Lorre@gmx.de>
2026-02-12 20:02:55 +0100

    Add files via upload

---
+++ bot/func.py
@@ -0,0 +1,15 @@
+import shelve
+
+class GuildsData:
+    def write(ctx, typ, content):
+        if(typ == "name"):
+            with shelve.open('Guilds_Data') as db:
+                db[ctx.guild.id, "name"] = {content}
+
+
+    def read(ctx, typ):
+        if(typ == "name"):
+            with shelve.open('Guilds_Data') as db:
+                content = db.get(ctx.guild.id, "name")
+        
+        return content
