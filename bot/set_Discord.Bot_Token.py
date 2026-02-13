d5ffb1b2dd3332e48116af9805860ea0360d3ec0
LorenzoL <leo.Lorre@gmx.de>
2026-02-13 18:26:08 +0100

    Remove Discord bot token input script

--- bot/set_Discord.Bot_Token.py
+++
@@ -1,9 +0,0 @@
-import shelve
-
-token = input("What is the token of your discord bot? ")
-token = int(token)
-
-with shelve.open('Discord.Bot_Token') as db:
-                db['token'] = {token}
-
-print(f"Your discord bot token is {token}")
