88d38c6dd92c43e37be6964f499c982025911be8
LorenzoL <leo.Lorre@gmx.de>
2026-02-13 18:25:16 +0100

    Refactor token handling in Discord_Interface.py
    Updated token retrieval method to read from a text file.

--- bot/Discord_Interface.py
+++ bot/Discord_Interface.py
@@ -2,16 +2,18 @@ import discord
 from discord.ext import commands
 import shelve
 
+prefix = '>' # <- Your prefix you want for your discord bot
+
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='>', intents=intents)
+bot = commands.Bot(command_prefix=prefix, intents=intents)
 
 @bot.command()
 async def start(ctx):
     print("Start")
 
 
-
-with shelve.open('Discord.Bot_Token') as db:
-    token = db.get("token")
+# read the token of your discord bot and use it
+with open("token.txt", "r") as file: # token.txt content = discord bot token
+    token = int(file.read())
 bot.run(token)
