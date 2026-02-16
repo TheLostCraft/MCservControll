913ec7e5d7c21c15307d157c878ebff79f4cb665
LorenzoL <leo.Lorre@gmx.de>
2026-02-16 18:48:36 +0100

    Update encryption_setup.py

--- bot/encryption_setup.py
+++ bot/encryption_setup.py
@@ -1,6 +1,10 @@
 from cryptography.fernet import Fernet
+import os
 
-with open("encrypt_key.txt", "w", encoding="utf-8") as datei:
-    datei.write(Fernet.generate_key().decode())
+if os.path.exists("encrypt_key.txt"):
+    print("The file 'encrypt_key.txt' already exists.")
+else:
+    with open("encrypt_key.txt", "w", encoding="utf-8") as datei:
+        datei.write(Fernet.generate_key().decode())
 
-print("Encrypt key setup complite")
+    print("Encrypt key setup complite")
